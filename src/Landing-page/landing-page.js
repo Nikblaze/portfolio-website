92ed5630985d75bb77a0b36a0f47d9b72b10c41d
Nikblaze <kumarnikhil237@gmail.com>
2024-09-07 22:46:03 +0530

    initial loading page animation

--- src/Landing-page/landing-page.js
+++ src/Landing-page/landing-page.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./landing-page.css";
-import profile from "../assets/profile.jpeg";
-import About from "../About-page/about.js";
-import Skills from "../Skills/skills.js";
+import React, { useEffect, useState } from 'react';
+import './landing-page.css';
+import profile from '../assets/profile.jpeg';
+import About from '../About-page/about.js';
+import Skills from '../Skills/skills.js';
 
-import Experience from "../Experience/experience";
-import Contacts from "../Contacts/contacts";
-import CommonConnectedIcons from "../commonConnectedIcons";
-import Projects from "../Projects/Projects";
-import { useRef } from "react";
+import Experience from '../Experience/experience';
+import Contacts from '../Contacts/contacts';
+import CommonConnectedIcons from '../commonConnectedIcons';
+import Projects from '../Projects/Projects';
+import { useRef } from 'react';
 
 function LandingPage() {
   const aboutRef = useRef(null);
@@ -16,79 +16,103 @@ function LandingPage() {
   const projectsRef = useRef(null);
   const contactRef = useRef(null);
   const experienceRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 4700);
+  }, []);
 
   const scrollToRef = (ref) => {
     window.scrollTo({
       top: ref.current.offsetTop,
-      behavior: "smooth",
+      behavior: 'smooth'
     });
   };
 
+  const message = 'Hi, I am Nikhil!';
+  const words = message.split(' ');
   return (
     <div>
-      <div className="first-page">
-        <div className="header">
-          <div className="header-options">
-            <span className="options" onClick={() => scrollToRef(aboutRef)}>
-              About
-            </span>
-          </div>
-          <div className="header-options">
-            <span className="options" onClick={() => scrollToRef(skillsRef)}>
-              Skills
-            </span>
-          </div>
-          <div className="header-options">
-            <span
-              className="options"
-              onClick={() => scrollToRef(experienceRef)}
-            >
-              Experience
-            </span>
-          </div>
-          <div className="header-options">
-            <span className="options" onClick={() => scrollToRef(projectsRef)}>
-              Projects
-            </span>
-          </div>
-          <div className="header-options">
-            <span className="options" onClick={() => scrollToRef(contactRef)}>
-              Contact
-            </span>
-          </div>
-        </div>
-        <div className="body">
-          <div className="profile-img">
-            <img src={profile} alt="profile" height={610} width={610} />
-          </div>
-          <div className="message">
-            <div className="title-message">
-              <div>
-                I'M<span className="name"> Nikhil Kumar</span>
-              </div>
-              <div className="job-title">Software Developer</div>
+      {loading ? (
+        <div className="loading-screen">
+          <div className="typing-container">
+          <div className="typing-text">
+              {words.map((word, index) => (
+                <span key={index} className={`word word-${index}`} style={{ '--i': index }}>
+                  {word}
+                </span>
+              ))}
             </div>
-            <CommonConnectedIcons />
           </div>
+          <div className="particles"></div>
+          <div className="circle-expansion"></div>
         </div>
-      </div>
-      <div className="second-page">
-        <section id="about" className="section" ref={aboutRef}>
-          <About />
-        </section>
-        <section id="skills" className="section" ref={skillsRef}>
-          <Skills />
-        </section>
-        <section id="projects" className="section" ref={projectsRef}>
-          <Projects />
-        </section>
-        <section id="experience" className="section" ref={experienceRef}>
-          <Experience />
-        </section>
-        <section id="contact" className="section" ref={contactRef}>
-          <Contacts />
-        </section>
-      </div>
+      ) : (
+        <>
+          <div className="first-page">
+            <div className="header">
+              <div className="header-options">
+                <span className="options" onClick={() => scrollToRef(aboutRef)}>
+                  About
+                </span>
+              </div>
+              <div className="header-options">
+                <span className="options" onClick={() => scrollToRef(skillsRef)}>
+                  Skills
+                </span>
+              </div>
+              <div className="header-options">
+                <span className="options" onClick={() => scrollToRef(experienceRef)}>
+                  Experience
+                </span>
+              </div>
+              <div className="header-options">
+                <span className="options" onClick={() => scrollToRef(projectsRef)}>
+                  Projects
+                </span>
+              </div>
+              <div className="header-options">
+                <span className="options" onClick={() => scrollToRef(contactRef)}>
+                  Contact
+                </span>
+              </div>
+            </div>
+            <div className="body">
+              <div className="profile-img">
+                <img src={profile} alt="profile" height={610} width={610} />
+              </div>
+              <div className="message">
+                <div className="title-message">
+                  <div>
+                    I'M<span className="name"> Nikhil Kumar</span>
+                  </div>
+                  <div className="job-title">Software Developer</div>
+                </div>
+                <CommonConnectedIcons />
+              </div>
+            </div>
+          </div>
+          <div className="second-page">
+            <section id="about" className="section" ref={aboutRef}>
+              <About />
+            </section>
+            <section id="skills" className="section" ref={skillsRef}>
+              <Skills />
+            </section>
+            <section id="projects" className="section" ref={projectsRef}>
+              <Projects />
+            </section>
+            <section id="experience" className="section" ref={experienceRef}>
+              <Experience />
+            </section>
+            <section id="contact" className="section" ref={contactRef}>
+              <Contacts />
+            </section>
+          </div>
+        </>
+      )}
     </div>
   );
 }
